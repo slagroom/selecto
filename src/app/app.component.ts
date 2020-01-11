77365e9ffeeca51e54d30d5821a6535c5f764323
slagroom <sl4gr00m@gmail.com>
2020-01-11 15:23:52 -0400

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,41 @@ export class AppComponent implements OnInit {
     { sport: 'football', name: 'Green Bay Packers' },
   ];
 
+  private sportDisplayName(sport: string) {
+    if (sport === 'hockey') {
+      return 'Hockey';
+    }
+    if (sport === 'basketball') {
+      return 'Basketball';
+    }
+    if (sport === 'baseball') {
+      return 'Baseball';
+    }
+    if (sport === 'football') {
+      return 'Football';
+    }
+    return sport;
+  }
+
+  private get sports(): { name: string; displayName: string }[] {
+    return Array.from(this.teams.map(t => t.sport)
+      .reduce((set, sport) => {
+        set.add(sport);
+        return set;
+      }, new Set<string>()))
+      .map(sport => {
+        return {
+          name: sport,
+          displayName: this.sportDisplayName(sport)
+        }
+      });
+  }
+
+  private initialFavorites: string[] = [
+    'Pittsburgh Penguins',
+    'Boston Celtics'
+  ];
+
   private form: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
@@ -58,6 +93,44 @@ export class AppComponent implements OnInit {
 
       ])
     });
+
+    this.form.get("favorites").valueChanges.subscribe(favorites => {
+
+      const usedSports = favorites
+        .map(f => f.sport)
+        .filter(f => f === null);
+
+      favorites.forEach((favorite) => {
+
+        const sport = favorite.sport;
+
+        favorite.sportChoices = this.sports
+          .filter(s => s.name === sport || !usedSports.includes(s.name));
+
+          favorite.teamChoices = this.teams
+          .filter(t => t.sport === sport)
+          .map(t => t.name);
+
+        if (!favorite.teamChoices.includes(favorite.team)) {
+          favorite.team = '';
+        }
+      });
+
+      // if (favorites[favorites.length - 1].sport !== null) {
+      //   favorites.push(
+      //     this.formBuilder.group({
+      //       sport: null,
+      //       team: null,
+      //       sportChoices: this.formBuilder.control(
+      //         this.sports.filter(s => !usedSports.includes(s.name))
+      //       ),
+      //       teamChoices: this.formBuilder.control([])
+      //     }));
+      // };
+
+      this.form.get("favorites").setValue(favorites, { emitEvent: false });
+    });
+
   }
 
   private log(msg: any) {
